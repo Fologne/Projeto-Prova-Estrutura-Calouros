f0c8db3b0b12a722c2c26fcaa2b804ab4692690f
fologne <gabrielfranfoloni01@gmail.com>
2025-10-03 18:43:35 -0400

    Implementacao do menu e futuras funcoes

---
+++ funcoes.h
@@ -0,0 +1,6 @@
+#include "struct.h"
+
+void cadastro(Node *lista);
+void listagem(Node *lista);
+void busca(Node *lista);
+void excluir(Node *lista);
--- menu.c
+++ menu.c
@@ -2,7 +2,33 @@
 #include <stdlib.h>
 #include "menu.h"
 #include "struct.h"
+#include "funcoes.h"
 
 void menu(){
-    
+    Node *lista = NULL;
+    int escolha;
+    do{
+        printf("Digite\n1 para cadastrar um novo contato\n2 para listar contatos\n3 para buscar contatos\n4 para excluir contatos\n0 para sair\n");
+        scanf("%i", &escolha);
+        switch (escolha){
+        case 1://cadastro = não feito
+            cadastro(lista);
+            break;
+        case 2://listagem = não feito
+            listagem(lista);
+            break;
+        case 3://busca = não feito
+            busca(lista);
+            break;
+        case 4://excluir = não feito
+            excluir(lista);
+            break;
+        case 0:
+            printf("Saindo...\n");
+            break;
+        default:
+            printf("Escolha invalida\n");
+            break;
+        }
+    }while(escolha != 0);
 }
